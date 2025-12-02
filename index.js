f398e8d3203c0737908b99bb56d5df655a049cd6
iaangelpruebas-ux <iaangelpruebas@gmail.com>
2025-12-02 09:34:06 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware para usar JSON
+app.use(express.json());
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.send('Servidor Node.js funcionando correctamente 😎 WOWOWO');
+});
+
+// Ruta de prueba tipo API
+app.get('/api/status', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'API lista y corriendo',
+    hora: new Date().toISOString()
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
